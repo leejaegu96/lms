c7afdffebc15d8e362541eb25923914f93922d01
jin7942 <jinbuem.kim@kakao.com>
2022-10-31 15:43:39 +0900

    Util 수정

---
+++ src/main/java/com/sumcofw/infra/common/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.sumcofw.infra.common.constants;
+
+public class Constants {
+    
+    /* for date */
+    public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+    public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
+    
+}
