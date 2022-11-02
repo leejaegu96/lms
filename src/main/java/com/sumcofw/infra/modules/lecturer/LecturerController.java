f4af1d77ab0d4631a172b8050cf796d5862da55e
jin7942 <jinbuem.kim@kakao.com>
2022-11-02 16:18:06 +0900

    A

--- src/main/java/com/sumcofw/infra/modules/lecturer/LecturerController.java
+++ src/main/java/com/sumcofw/infra/modules/lecturer/LecturerController.java
@@ -1,12 +1,7 @@
 package com.sumcofw.infra.modules.lecturer;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 
 @Controller
 @RequestMapping(value = "/lecturer")
